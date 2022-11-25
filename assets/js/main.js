f896386adfe3b894cb2543c1465eb876f6685424
ospersa <49665885+ospersa@users.noreply.github.com>
2022-11-24 23:54:56 -0500

    Finalización Interactivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,4 +13,26 @@ $(document).ready(function() {
         keyboardScrolling: false,
     });
     $.fn.pagepiling.setAllowScrolling(false)
+    $('#mapa').load('../assets/img/mapa.svg')
+    $('.hoverable').hover(function() {
+        console.log("Foasd")
+    })
+    $('.content-box-header').on('click', function() {
+        $(this).removeClass('animate__pulse')
+        const content = $(this).data('content');
+        $(`#${content}`).css('opacity', '1').addClass('animate__fadeInDown')
+    })
 })
+
+function addClassBlue (element) {
+    $('.' + element).addClass('blue')
+}
+
+function removeClassBlue (element) {
+    $('.' + element).removeClass('blue')
+}
+
+function show (element) {
+    $('.location.' + element).addClass('animate__swing')
+    $('.location.' + element).css('opacity', '1')
+}
